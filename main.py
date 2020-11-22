8f4459a79b56cbf0dd7cb85d1a80c60b123c0cf5
Ivan Krasilnikov <krasivan93@gmail.com>
2020-11-22 16:51:54 +0200

    Working

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
-from typing import Optional
+from typing import Optional, Dict
+from microservice import Product
 import microservice
-from fastapi import FastAPI
-from pydantic import BaseModel
 import uvicorn
 from fastapi.encoders import jsonable_encoder
-from fastapi import APIRouter, Body
-from bson import ObjectId, json_util
-from microservice import Product
+from fastapi import APIRouter, Body, FastAPI
+from fastapi.responses import Response
 
 app = FastAPI()
 router = APIRouter()
 
 
-@app.get("/retrieve_product")
-def find_product(name: Optional[str],  options: Optional[list]):
-    product = microservice.find_product()
-    return product
+@app.get("/retrieve_productname_by_name")
+def retrieve_productname_by_name(name: Optional[str] = None):
+    products_filtered = microservice.retrieve_productname_by_name(name)
+    return Response(content=products_filtered, media_type="application/json")
+
+
+@app.get("/retrieve_product_by_name")
+def retrieve_product_by_name(name: Optional[str] = None):
+    products_filtered = microservice.retrieve_product_by_name(name)
+    return Response(content=products_filtered, media_type="application/json")
+
+
+@app.get("/retrieve_product_by_id")
+def retrieve_product_by_id(_id: str):
+    products_filtered = microservice.retrieve_product_by_id(_id)
+    return Response(content=products_filtered, media_type="application/json")
+
+
+@app.post("/retrieve_productname_by_options")
+def retrieve_productname_by_options(options: Optional[Dict] = Body(None)):
+    products_filtered = microservice.retrieve_productname_by_options(options)
+    return Response(content=products_filtered, media_type="application/json")
 
 
 @app.post("/create")
 def create_product(product: Product = Body(..., example={
-                "name": "Iphone11",
-                "description": "Phone from Apple",
-                "options": [
-                    {"os": "Ios"},
-                    {"year": "2018"}
-                ],
-            }, embed=True)):
+    "name": "Iphone11",
+    "description": "Phone from Apple",
+    "options": {"os": "Ios",
+                "year": "2018"},
+}, embed=True)):
     print(product)
     product = jsonable_encoder(product)
     print(product)
     new_product = microservice.create_product(product)
-    return new_product
-
-
-# @app.post("/create")
-# def create_product(product: Product):
-#     print(product)
-#     product = json_util.loads(product)
-#     print(product)
-#     new_product = microservice.create_product(product)
-#     return new_product
+    return Response(content=new_product, media_type="application/json")
 
 
 if __name__ == "__main__":
--- microservice.py
+++ microservice.py
@@ -1,22 +1,17 @@
-from typing import Optional, List, Union
+from typing import Optional, Dict
 from pydantic import BaseModel
 from pymongo import MongoClient
-from bson import ObjectId, json_util
-from fastapi import Body
+from bson import ObjectId, json_util, SON
 
 cluster = MongoClient('localhost', 27017)
 db = cluster['mservice']
 collection = db['product']
 
 
-# class Options(BaseModel):
-#     key: Optional[str]
-
-
 class Product(BaseModel):
     name: str
     description: Optional[str] = None
-    options: Optional[list]
+    options: Optional[Dict]
 
 
 def create_product(product):
@@ -27,7 +22,37 @@ def create_product(product):
     return json_util.dumps(new_product)
 
 
-def find_product():
-    product = collection.find_one({})
+def retrieve_productname_by_name(name):
+    if name:
+        product = collection.find({"name": name})
+    else:
+        product = collection.find({})
+    filtered_products = list(product)
+    result = set()
+    for product in filtered_products:
+        result.add(product['name'])
+    return json_util.dumps(result)
+
+
+def retrieve_productname_by_options(options):
+    product = collection.find(SON({"options": options}))
+    filtered_products = list(product)
+    result = set()
+    for product in filtered_products:
+        result.add(product['name'])
+    return json_util.dumps(result)
+
+
+def retrieve_product_by_id(_id):
+    product = collection.find_one(SON({"_id": ObjectId(_id)}))
     print(product)
     return json_util.dumps(product)
+
+
+def retrieve_product_by_name(name):
+    if name:
+        product = collection.find({"name": name})
+    else:
+        product = collection.find({})
+    filtered_products = list(product)
+    return json_util.dumps(filtered_products)
